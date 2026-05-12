bd57fa29c3b1fbc1f97330423ec7279fa6e0a0cd
Cristian Flores <cfloresp5@miumg.edu.gt>
2026-05-11 19:23:36 -0600

    feat: definir interfaz del motor y estrategia custom inicial

---
+++ tree-engine/src/main/java/com/p3/engine/CustomTreeStrategy.java
@@ -0,0 +1,12 @@
+package com.p3.engine;
+
+public class CustomTreeStrategy implements TreeAlgorithmStrategy {
+
+    @Override
+    public String getEngineType() {
+        return "Motor de Árbol Personalizado (Integrante A) - Semana 1";
+    }
+    
+    // Aquí es donde en la Semana 2 programarás 
+    // la lógica de nodos con tus propios punteros
+}
